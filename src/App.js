3bcb04cab1dd9c5027d141157d76a084197795bc
GeethalakshmiB <kavyageetha47@gmail.com>
2024-01-24 06:02:01 +0530

    Folder try

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+//REGISTER Field - to track of the changes in the input field by passing this attribute
+
+//DEFAULT REACT APP.JS
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +25,572 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+
+//WITHOUT REACT HOOK FORM
+/*import React, { useState } from "react";
+import './App.css';
+
+export default function App() {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevProps) => ({
+      ...prevProps,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(state);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={state.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}*/
+
+//WITH REACT FORM
+/*import React from "react";
+import { useForm } from "react-hook-form";
+import "./styles.css";
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Email</label>
+          <input type="text" name="email" {...register("email")} />
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input type="password" name="password" {...register("password")} />
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}*/
+
+
+//WITH REACT Hook Form and with external Validation
+/*import React from "react";
+import { useForm } from "react-hook-form";
+import "./styles.css";
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            {...register("email", {
+              required: true,
+              pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
+            })}
+          />
+          {errors.email && errors.email.type === "required" && (
+            <p className="errorMsg">Email is required.</p>
+          )}
+          {errors.email && errors.email.type === "pattern" && (
+            <p className="errorMsg">Email is not valid.</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: true,
+              minLength: 6
+            })}
+          />
+          {errors.password && errors.password.type === "required" && (
+            <p className="errorMsg">Password is required.</p>
+          )}
+          {errors.password && errors.password.type === "minLength" && (
+            <p className="errorMsg">
+              Password should be at-least 6 characters.
+            </p>
+          )}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}*/
+
+//With React Hook Form and Inside validation
+/*import React from "react";
+import { useForm } from "react-hook-form";
+import "./styles.css";
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Email is not valid."
+              }
+            })}
+          />
+          {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: true,
+              validate: {
+                checkLength: (value) => value.length >= 6,
+                matchPattern: (value) =>
+                  /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])/.test(
+                    value
+                  )
+              }
+            })}
+          />
+          {errors.password?.type === "required" && (
+            <p className="errorMsg">Password is required.</p>
+          )}
+          {errors.password?.type === "checkLength" && (
+            <p className="errorMsg">
+              Password should be at-least 6 characters.
+            </p>
+          )}
+          {errors.password?.type === "matchPattern" && (
+            <p className="errorMsg">
+              Password should contain at least one uppercase letter, lowercase
+              letter, digit, and special symbol.
+            </p>
+          )}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}*/
+
+//USER REGISTRATION and RESET to clear the form contents after submission
+/*import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./styles.css";
+
+export default function App() {
+  const [successMsg, setSuccessMsg] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset // this to clear the form content aftr submission
+} = useForm();
+
+const onSubmit = (data) => {
+  console.log(data);
+  setSuccessMsg("User registration is successful.");
+  reset();
+};
+
+return (
+  <div className="App">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {successMsg && <p className="success-msg">{successMsg}</p>}
+      <div className="form-control">
+        <label>Username</label>
+        <input
+          type="text"
+          {...register("username", {
+            required: "Username is required."
+          })}
+        />
+        {errors.username && (
+          <p className="errorMsg">{errors.username.message}</p>
+        )}
+      </div>
+      <div className="form-control">
+        <label>Email</label>
+        <input
+          type="text"
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: "Email is not valid."
+            }
+          })}
+        />
+        {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+      </div>
+      <div className="form-control">
+        <label>Password</label>
+        <input
+          type="password"
+          {...register("password", {
+            required: true,
+            validate: {
+              checkLength: (value) => value.length >= 6,
+              matchPattern: (value) =>
+                /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])/.test(
+                  value
+                )
+            }
+          })}
+        />
+        {errors.password?.type === "required" && (
+          <p className="errorMsg">Password is required.</p>
+        )}
+        {errors.password?.type === "checkLength" && (
+          <p className="errorMsg">
+            Password should be at-least 6 characters.
+          </p>
+        )}
+        {errors.password?.type === "matchPattern" && (
+          <p className="errorMsg">
+            Password should contain at least one uppercase letter, lowercase
+            letter, digit, and special symbol.
+          </p>
+        )}
+      </div>
+      <div className="form-control">
+        <label></label>
+        <button type="submit">Register</button>
+      </div>
+    </form>
+  </div>
+);
+}*/
+
+//React Hook form with other libraries - COntroller library
+/*import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+import "./styles.css";
+
+const departments = [
+  { value: "Computer-Science", label: "Computer Science" },
+  { value: "Physics", label: "Physics" },
+  { value: "Chemistry", label: "Chemistry" },
+  { value: "Mathematics", label: "Mathematics" }
+];
+
+export default function App() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Select Department of Interest</label>
+          <Controller
+            name="department"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Select {...field} isMulti options={departments} />
+            )}
+          />
+          {errors.department && (
+            <p className="errorMsg">This is a required field.</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}*/
+
+//WIth Other Input Types - textbox, radio button and checkbox
+/*import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import "./styles.css";
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", {
+              required: "Please enter your email",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Please enter a valid email"
+              }
+            })}
+          />
+          {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="gender">
+          <Form.Label>Select Gender</Form.Label>
+          <Form.Check
+            type="radio"
+            label="Male"
+            {...register("gender", {
+              required: "Please select your gender"
+            })}
+          />
+          <Form.Check
+            type="radio"
+            label="Female"
+            value="female"
+            {...register("gender")}
+          />
+          {errors.gender && <p className="errorMsg">{errors.gender.message}</p>}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="skills">
+          <Form.Label>Select Your Skills</Form.Label>
+          <Form.Check
+            type="checkbox"
+            label="JavaScript"
+            value="JavaScript"
+            {...register("skills", {
+              required: "Please select at-least one skill"
+            })}
+          />
+          <Form.Check
+            type="checkbox"
+            label="React"
+            value="react"
+            {...register("skills")}
+          />
+          <Form.Check
+            type="checkbox"
+            label="Node.js"
+            value="nodejs"
+            {...register("skills")}
+          />
+          <Form.Check
+            type="checkbox"
+            label="Angular"
+            value="angular"
+            {...register("skills")}
+          />
+          {errors.skills && <p className="errorMsg">{errors.skills.message}</p>}
+        </Form.Group>
+        <label></label>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}*/
+
+//To set default values at checkbox and radio button
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import "./styles.css";
+
+const initialValues = {
+  gender: "female",
+  skills: {
+    JavaScript: true,
+    react: false,
+    nodejs: true,
+    angular: false
+  }
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      gender: initialValues.gender,
+      skills: Object.keys(initialValues.skills).filter(
+        (item) => initialValues.skills[item] === true
+      )
+    }
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", {
+              required: "Please enter your email",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Please enter a valid email"
+              }
+            })}
+          />
+          {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="gender">
+          <Form.Label>Select Gender</Form.Label>
+          <Form.Check
+            type="radio"
+            label="Male"
+            value="male"
+            {...register("gender", {
+              required: "Please select your gender"
+            })}
+          />
+          <Form.Check
+            type="radio"
+            label="Female"
+            value="female"
+            {...register("gender")}
+          />
+          {errors.gender && <p className="errorMsg">{errors.gender.message}</p>}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="skills">
+          <Form.Label>Select Your Skills</Form.Label>
+          <Form.Check
+            type="checkbox"
+            label="JavaScript"
+            value="JavaScript"
+            {...register("skills", {
+              required: "Please select at-least one skill"
+            })}
+          />
+          <Form.Check
+            type="checkbox"
+            label="React"
+            value="react"
+            {...register("skills")}
+          />
+          <Form.Check
+            type="checkbox"
+            label="Node.js"
+            value="nodejs"
+            {...register("skills")}
+          />
+          <Form.Check
+            type="checkbox"
+            label="Angular"
+            value="angular"
+            {...register("skills")}
+          />
+          {errors.skills && <p className="errorMsg">{errors.skills.message}</p>}
+        </Form.Group>
+        <label></label>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
